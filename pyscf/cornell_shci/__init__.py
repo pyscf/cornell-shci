3e717c90cba32dbe17ad66411dab290f326a9733
Qiming Sun <osirpt.sun@gmail.com>
2021-02-27 13:59:42 -0800

    Move pyscf.cornell_shci to extended projects
    (cherry picked from commit e55aa502086542b8a3d05ef8c44ce3bdc73b9b77)

--- pyscf/cornell_shci/__init__.py
+++ pyscf/cornell_shci/__init__.py
@@ -13,5 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pyscf.cornell_shci.shci import SHCI, SHCISCF
+__version__ = '0.0.1'
 
+from pyscf.cornell_shci.shci import SHCI, SHCISCF
